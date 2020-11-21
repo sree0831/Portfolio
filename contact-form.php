0d0d8036bfd6f954d5309fb2ec966bcf89b420a0
sree0831 <sreelakshmi.passavula@gmail.com>
2020-11-21 21:07:18 +0000

    contact

--- contact-form.php
+++ contact-form.php
@@ -17,8 +17,8 @@ if (isset($_POST['inputName']) && isset($_POST['inputEmail'])&& isset($_POST['in
 
     $mail->From = $_POST['inputEmail'];
     $mail->FromName = $_POST['inputName'];
-    $mail->AddAddress('your@email.here'); //recipient 
-    $mail->Subject = 'Enquiry from Hencework';
+    $mail->AddAddress('sreelakshmi.passavula@gmail.com'); //recipient 
+    $mail->Subject = 'Enquiry from Sree';
     $mail->Body = "Name: " . $_POST['inputName'] . "\r\n\r\nMessage: " . stripslashes($_POST['inputMessage']);
 
     if (isset($_POST['ref'])) {
