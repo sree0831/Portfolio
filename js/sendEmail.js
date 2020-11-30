250c8d537ee9a5196a14c1e67fd49b8972ff528a
sree0831 <sreelakshmi.passavula@gmail.com>
2020-11-30 21:04:12 +0000

    email

---
+++ js/sendEmail.js
@@ -0,0 +1,25 @@
+
+
+import React from 'react';
+import emailjs from 'emailjs-com';
+
+
+
+
+
+  function sendMail(contactForm) {
+    emailjs.send("gmail", "sree", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
